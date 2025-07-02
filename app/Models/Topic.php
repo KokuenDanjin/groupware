d06c6a3d6ea2c7741bcebd1e9afc087a3c0be286
Yoshiki Yokoyama <y.yokoyfb@gmail.com>
2025-07-02 16:51:27 +0900

    現在の作業をコミット

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Topic extends Model
 {
-    //
+    use SoftDeletes;
+
+    protected $fillable = ['user_id', 'title', 'body'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
