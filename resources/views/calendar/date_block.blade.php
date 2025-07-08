bbd3f4767e87c332771a757e55150bf79b0e339a
jounouchi666 <trueguy666@gmail.com>
2025-07-08 14:50:50 +0900

    リンクの日付情報をクエリパラメータ化

--- resources/views/calendar/date_block.blade.php
+++ resources/views/calendar/date_block.blade.php
@@ -1,33 +1,46 @@
 <div class="calendar-date-block">
     @php
-        $queryParam = '?' . http_build_query(['userId' => $userId]);
+        $baseQueryParams = ['userId' => $userId];
+        $getQueryParam = fn($addingParam = []) => '?' . http_build_query([...$baseQueryParams, ...$addingParam]);
     @endphp
     <span>
-        <a class="calendar-move-button" href="{{ route('calendar.view', ['type' => $type, 'currentDate' => $calendar->getBeforeWeek()]) . $queryParam }}">
+        <a
+            class="calendar-move-button"
+            href="{{ route('calendar.view', ['type' => $type]) . $getQueryParam(['currentDate' => $type ==='month' ? \App\Calendar\CalendarView::getBeforeMonth($currentDate) : \App\Calendar\CalendarView::getBeforeWeek($currentDate)]) }}"
+        >
             <span class="material-symbols-outlined">keyboard_double_arrow_left</span>
         </a>
     </span>
     @if ($type !== 'month')
     <span>
-        <a class="calendar-move-button" href="{{ route('calendar.view', ['type' => $type, 'currentDate' => $calendar->getBeforeDay()]) . $queryParam }}">
+        <a
+            class="calendar-move-button"
+            href="{{ route('calendar.view', ['type' => $type]) . $getQueryParam(['currentDate' => \App\Calendar\CalendarView::getBeforeDay($currentDate)]) }}"
+        >
             <span class="material-symbols-outlined">keyboard_arrow_left</span>
         </a>
     </span>
     @endif
     <span class="calendar-date">
-        <a href="{{ route('calendar.view', ['type' => $type, 'currentDate' => \Carbon\Carbon::now()->format('Y-m-d') ]) . $queryParam }}">
+        <a href="{{ route('calendar.view', ['type' => $type]) . $getQueryParam(['currentDate' => now()->toDateString('Y-m-d')]) }}">
             {{ $type === 'month' ? $calendar->getTitle() : $calendar->getCurrentDayString() }}
         </a>
     </span>
     @if ($type !== 'month')
     <span>
-        <a class="calendar-move-button" href="{{ route('calendar.view', ['type' => $type, 'currentDate' => $calendar->getNextDay()]) . $queryParam }}">
+        <a
+            class="calendar-move-button"
+            href="{{ route('calendar.view', ['type' => $type]) . $getQueryParam(['currentDate' => \App\Calendar\CalendarView::getNextDay($currentDate)]) }}"
+        >
             <span class="material-symbols-outlined">keyboard_arrow_right</span>
         </a>
     </span>
     @endif
     <span>
-        <a class="calendar-move-button" href="{{ route('calendar.view', ['type' => $type, 'currentDate' => $calendar->getNextWeek()]) . $queryParam }}">
+        <a
+            class="calendar-move-button"
+            href="{{ route('calendar.view', ['type' => $type]) . $getQueryParam(['currentDate' => $type ==='month' ? \App\Calendar\CalendarView::getNextMonth($currentDate) : \App\Calendar\CalendarView::getNextWeek($currentDate)]) }}"
+        >
             <span class="material-symbols-outlined">keyboard_double_arrow_right</span>
         </a>
     </span>
